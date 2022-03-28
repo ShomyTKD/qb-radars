5e9663ec2c6c0614adc926866541fc3b9cd81394
Shomy <26857315+ShomyTKD@users.noreply.github.com>
2022-03-28 18:20:18 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.RadarCost = 50
 
 Config.Radars = {
-	vector4(406.89, -969.06, 29.43, 33.0), -- MRPD
+    vector4(406.89, -969.06, 29.43, 33.0), -- MRPD
     vector4(246.63, -1028.84, 28.64, 123.28), -- legion Square
     vector4(129.37, -989.91, 28.7, 118.35), -- legion Square
     vector4(187.31, -842.44, 30.4, 30.81), -- legion Square
